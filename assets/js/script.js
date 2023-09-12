bb78bb22c4f2076770a9805a3417fac9bd8ff94f
Mamunur Rahman <mamunur@mac-air.home>
2023-09-12 22:47:03 +0100

    hour update past present future added

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,5 +26,30 @@ $(document).ready(function(){
     })
 
 
+    function hourUpdate(){
+        let currentHour = moment().hours();
+        
+
+        for(let i = 0; i < $(".time-block").length; i++){
+            let hour = parseInt($(".time-block")[i].getAttribute("id").split("-")[1])
+            console.log(hour)
+            console.log(currentHour)
+            if(hour < currentHour) {
+                $(".time-block")[i].classList.add("past")
+            } else if(hour === currentHour){
+                $(".time-block")[i].classList.add("past")
+                $(".time-block")[i].classList.remove("present")
+            } else {
+                $(".time-block")[i].classList.remove("past")
+                $(".time-block")[i].classList.remove("present") 
+                $(".time-block")[i].classList.add("future")
+            }
+        }
+
+    }
+
+    
+    hourUpdate();
+
 
 })
